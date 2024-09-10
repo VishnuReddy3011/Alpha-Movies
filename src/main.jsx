09d1c3302c93492853da8aa22742e6d962945f21
VishnuReddy3011 <sajjalavishnuvardhanreddy2001@gmail.com>
2024-09-10 23:23:49 +0530

    watchlis loading bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
 import App from './App.jsx';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
