9c2cb42468c5c5b8095bc3403c44eab0f90825e1
VishnuReddy3011 <sajjalavishnuvardhanreddy2001@gmail.com>
2024-09-10 19:40:33 +0530

    Completed all watchlist features

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -71,7 +71,11 @@ const WatchList = React.memo(() => {
     };
   }, [text]); // Only run the effect when `text` changes
 
+  const [sortVal, setSortVal] = useState("0");
   const [sortOpt, setSortOpt] = useState("Default");
+  const [titleOrder, setTitleOrder] = useState(true);
+  const [imdbOrder, setImdbOrder] = useState(false);
+  const [popuOrder, setPopuOrder] = useState(true); 
 
   return (
     <>
@@ -82,7 +86,7 @@ const WatchList = React.memo(() => {
         id="watchlist-search-input"
         onChange={(e) => setText(e.target.value.toLowerCase())}
       />
-      <div className="absolute top-24 right-[300px] text-white w-[700px] h-[395px] flex flex-col items-end">
+      <div className="absolute top-[98px] right-[300px] text-white w-[700px] h-[390px] flex flex-col items-end">
         <div 
           className="flex gap-2 items-center hoverDiv cursor-pointer"
           onMouseEnter={() => setDisplayVal("100%")}
@@ -107,9 +111,88 @@ const WatchList = React.memo(() => {
           </ul>
         </div>
       </div>
-      <div className="text-white absolute top-24 right-[100px] p-[10px] flex justify-center items-center hoverDiv gap-2">
-        <span>Sort by : {sortOpt}</span>
-        <i className="fa-solid fa-angle-down mt-1"></i>
+      <div className="text-white absolute top-[88px] right-[100px] p-[10px] h-[220px] flex flex-col items-end">
+        <div 
+          className="flex justify-center items-center hoverDiv gap-2 cursor-pointer"
+          onMouseEnter={() => setSortVal("100%")}
+          onMouseLeave={() => setSortVal("0")}
+        >
+          <span>Sort by : {sortOpt}</span>
+          <i className="fa-solid fa-angle-down mt-1"></i>
+        </div>
+        <div 
+          className="w-[120px] ulTag flex my-[20px]"
+          style={{height: `${sortVal}`}}
+          onMouseEnter={() => setSortVal("100%")}
+          onMouseLeave={() => setSortVal("0")}
+        >
+          <ul 
+            className="cursor-pointer sort-ulTag"
+            onClick={e => {
+              const children = e.currentTarget.childNodes;
+              for(let i = 0; i < children.length; i++) {
+                if(i == 0) {
+                  children[i].style.color = "gold";
+                }
+                else {
+                  children[i].style.color = "white";
+                }
+              }
+              let prevOpt = sortOpt;
+              let sortText = e.target.textContent || e.target.parentNode.firstChild.textContent;
+              let parent = e.target.parentNode;
+              if(sortText === "Title") {
+                if(prevOpt === sortText) {
+                  setTitleOrder(prev => !prev);
+                }
+              }
+              else if(sortText === "IMDB") {
+                if(prevOpt === sortText) {
+                  setImdbOrder(prev => !prev);
+                }
+              }
+              else if(sortText === "Popularity") {
+                if(prevOpt === sortText) {
+                  setPopuOrder(prev => !prev);
+                }
+              }
+              setSortOpt(sortText);
+              parent.style.color = "red";
+            }}
+          >
+            <li style={{color: "gold"}}>Default</li>
+            <li>
+              <span>Title</span>
+              {
+                titleOrder
+                  ?
+                <i className="fa-solid fa-arrow-up ml-1"></i>
+                  :
+                <i className="fa-solid fa-arrow-down ml-1"></i>
+              }
+            </li>
+            <li>
+              <span>IMDB</span>
+              {
+                imdbOrder
+                  ?
+                <i className="fa-solid fa-arrow-up ml-1"></i>
+                  :
+                <i className="fa-solid fa-arrow-down ml-1"></i>
+              }
+            </li>
+            <li>
+              <span>Popularity</span>
+              {
+                popuOrder
+                  ?
+                <i className="fa-solid fa-arrow-up ml-1"></i>
+                  :
+                <i className="fa-solid fa-arrow-down ml-1"></i>
+              }
+            </li>
+          </ul>
+        </div>
       </div>
       <table className="watch-list mb-10 -mt-5">
         <thead>
@@ -121,7 +204,7 @@ const WatchList = React.memo(() => {
             <th></th>
           </tr>
         </thead>
-        <tbody>{getTbody(watchList, removeFromWatchList, debouncedText, genreVal, genreids)}</tbody>
+        <tbody>{getTbody(watchList, removeFromWatchList, debouncedText, genreVal, genreids, sortOpt, titleOrder, imdbOrder)}</tbody>
       </table>
     </>
   );
@@ -129,8 +212,32 @@ const WatchList = React.memo(() => {
 
 export default WatchList;
 
-function getTbody(watchList, removeFromWatchList, text, genreVal, genreids) { // useCallback
+function getTbody(watchList, removeFromWatchList, text, genreVal, genreids, sortOpt, titleOrder, imdbOrder) { // useCallback
   // watchList = new Map([...watchList.entries()].sort((a, b) => a[1][0].vote_average.toFixed(1) - b[1][0].vote_average.toFixed(1)))
+  if(sortOpt !== "Default") {
+    if(sortOpt === "Title") {
+      watchList = new Map(
+        [...watchList.entries()].sort(
+          (a, b) => {
+            let title1 = (a[1][0].title || a[1][0].name);
+            let title2 = (b[1][0].title || b[1][0].name);            
+            if(titleOrder) {
+              return title1.localeCompare(title2);
+            }
+            return title2.localeCompare(title1);
+          }
+        )
+      )
+    }
+    else if(sortOpt === "IMDB") {
+      watchList = new Map([...watchList.entries()].sort((a, b) => {
+        if(imdbOrder) {
+          return b[1][0].vote_average.toFixed(1) - a[1][0].vote_average.toFixed(1)
+        }
+        return a[1][0].vote_average.toFixed(1) - b[1][0].vote_average.toFixed(1);
+      }));
+    }
+  }
   const iterator = watchList[Symbol.iterator]();
   const res = [];
   for (const [_, [movieObj, isMovie]] of iterator) {
